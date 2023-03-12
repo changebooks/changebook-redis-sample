f38ff951d60b64440d900856a8ea48a4381a14c9
changebooks <changebooks@qq.com>
2023-03-12 22:54:04 +0800

    1.1.4

--- README.md
+++ README.md
@@ -7,19 +7,22 @@
 http://127.0.0.1:8080/city/select-one?id=1
 
 // 删除缓存
-http://127.0.0.1:8080/city/update-one?id=1&cityName=BJ
+http://127.0.0.1:8080/city/update-one?id=1&cityName=bj
 ```
 
 ### 分布式锁
 ```
 // 加锁
-http://127.0.0.1:8080/cache-lock/lock?expirationTime=100
+http://127.0.0.1:8080/cache-lock/lock?expirationTime=30
 
 // 解锁
 http://127.0.0.1:8080/cache-lock/unlock
 
 // 续期
-http://127.0.0.1:8080/cache-lock/renewal?expirationTime=100
+http://127.0.0.1:8080/cache-lock/renewal?expirationTime=30
+
+// 定时续期
+http://127.0.0.1:8080/cache-lock/schedule-renewal?delayTime=10&expirationTime=30
 ```
 
 ### 分布式限流，固定时间窗口，x秒内，许可n次
--- src/main/java/io/github/changebooks/redis/sample/CacheLockController.java
+++ src/main/java/io/github/changebooks/redis/sample/CacheLockController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * 分布式锁
@@ -22,14 +22,23 @@ public class CacheLockController {
     private CacheLock cacheLock;
 
     /**
+     * 加锁、解锁、续期，在同一线程
+     */
+    private static final ExecutorService THREAD_EXECUTOR = Executors.newSingleThreadExecutor();
+
+    /**
      * 加锁
      *
      * @param expirationTime 过期时间
      * @return 加锁成功？
      */
     @GetMapping(value = "/lock")
-    public Boolean lock(@RequestParam("expirationTime") Long expirationTime) {
-        return cacheLock.lock(expirationTime, TimeUnit.SECONDS);
+    public Boolean lock(@RequestParam("expirationTime") Long expirationTime)
+            throws ExecutionException, InterruptedException {
+        FutureTask<Boolean> task = new FutureTask<>(() -> cacheLock.lock(expirationTime, TimeUnit.SECONDS));
+
+        THREAD_EXECUTOR.execute(task);
+        return task.get();
     }
 
     /**
@@ -38,8 +47,11 @@ public class CacheLockController {
      * @return 解锁成功？
      */
     @GetMapping(value = "/unlock")
-    public Boolean unlock() {
-        return cacheLock.unlock();
+    public Boolean unlock() throws ExecutionException, InterruptedException {
+        FutureTask<Boolean> task = new FutureTask<>(() -> cacheLock.unlock());
+
+        THREAD_EXECUTOR.execute(task);
+        return task.get();
     }
 
     /**
@@ -49,8 +61,23 @@ public class CacheLockController {
      * @return 续期成功？
      */
     @GetMapping(value = "/renewal")
-    public Boolean renewal(@RequestParam("expirationTime") Long expirationTime) {
-        return cacheLock.renewal(expirationTime, TimeUnit.SECONDS);
+    public Boolean renewal(@RequestParam("expirationTime") Long expirationTime)
+            throws ExecutionException, InterruptedException {
+        FutureTask<Boolean> task = new FutureTask<>(() -> cacheLock.renewal(expirationTime, TimeUnit.SECONDS));
+
+        THREAD_EXECUTOR.execute(task);
+        return task.get();
+    }
+
+    /**
+     * 定时续期
+     *
+     * @param delayTime      延迟时间
+     * @param expirationTime 过期时间
+     */
+    @GetMapping(value = "/schedule-renewal")
+    public void scheduleRenewal(@RequestParam("delayTime") Long delayTime, @RequestParam("expirationTime") Long expirationTime) {
+        THREAD_EXECUTOR.execute(() -> cacheLock.scheduleRenewal(delayTime, expirationTime, TimeUnit.SECONDS));
     }
 
 }
